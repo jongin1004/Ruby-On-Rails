afdcf59dabfa40f3222e705f32262ada58e82900
bill1224 <bill1224@naver.com>
2021-09-08 22:46:40 +0900

    comment -> delete

--- rails_study/app/controllers/comments_controller.rb
+++ rails_study/app/controllers/comments_controller.rb
@@ -5,7 +5,12 @@ class CommentsController < ApplicationController
         redirect_to scaffold_blog_path(@scaffold_blog)
     end
 
-    
+    def destroy
+        @scaffold_blog = ScaffoldBlog.find(params[:scaffold_blog_id])
+        @comment = @scaffold_blog.comments.find(params[:id])
+        @comment.destroy
+        redirect_to scaffold_blog_path(@scaffold_blog) 
+    end
     private
     def comment_params
        params.require(:comment).permit(:comment) 
