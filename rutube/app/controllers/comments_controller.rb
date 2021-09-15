a2ca628563ad9fc6be87512d47cc491df3a0de5a
bill1224 <bill1224@naver.com>
2021-09-15 22:31:47 +0900

    add comments using Ajax and Jquery

---
+++ rutube/app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @comment = current_user.comments.create(comment_params)
+        @video = @comment.video
+    end
+
+    def destroy
+        @comment = current_user.comments.find params[:id]
+        @comment.destroy
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:body, :video_id)
+    end
+end
--- rutube/app/models/video.rb
+++ rutube/app/models/video.rb
@@ -2,4 +2,5 @@ class Video < ApplicationRecord
   mount_uploader :file, FileUploader
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
